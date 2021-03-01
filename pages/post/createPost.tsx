507c70dd9a57769fb4ca19764d8ea4c5d51dcf81
RYOTA <kobaryo04ysh@icloud.com>
2021-03-02 00:00:48 +0900

    feat: 投稿フォームを別ページで表示するためのcomponent

---
+++ pages/post/createPost.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PostForm from "../../components/PostForm";
+import "../../styles/Main.module.css";
+
+export default function createPost() {
+  return (
+    <div>
+      <PostForm />
+    </div>
+  );
+}
