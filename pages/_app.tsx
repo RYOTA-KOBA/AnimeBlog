37e8a97a95454bb8cf4845fa604868b4b5aa81c0
RYOTA <kobaryo04ysh@icloud.com>
2021-03-12 20:27:15 +0900

    feat: contextの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import { PostProvider } from "../components/context/PostContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PostProvider>
+      <Component {...pageProps} />
+    </PostProvider>
+  );
 }
 
 export default MyApp;
